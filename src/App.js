03837537e031e7f02f1bd4bf05e2f1602f9e33c8
mithrax <rahul.anthonym@gmail.com>
2020-10-12 13:53:43 -0400

    setup axios get request and preliminary testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
+const axios = require("axios");
+
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
+
+  axios
+    .get("http://localhost:3001/candidates")
+    .then((res) => {
+      let data = res.data;
+      data.forEach((e) => {
+        console.log(`${e.first_name}, ${e.last_name}, ${e.email}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
   const getCandidates = (date) => {
     setSelectedDate(date);
@@ -21,7 +35,7 @@ function App() {
         onChange={(date) => getCandidates(date)}
         // onChange={(date) => setSelectedDate(date)}
         minDate={new Date()}
-        isClearable
+        // isClearable
         showMonthDropdown
         scrollableMonthYearDropdown
         placeholderText="Click to select a date"
