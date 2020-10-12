e9bc8ed65c4e0b8bb17221e23f8a754dcd99d9e5
mithrax <rahul.anthonym@gmail.com>
2020-10-12 13:42:54 -0400

    install json server and create json db file store api data

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import "./App.css";
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
 
+  const getCandidates = (date) => {
+    setSelectedDate(date);
+    console.log(date);
+    console.log(date.getDate());
+    console.log(date.toDateString());
+  };
+
   return (
     <div className="App">
       <DatePicker
         selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
+        onChange={(date) => getCandidates(date)}
+        // onChange={(date) => setSelectedDate(date)}
         minDate={new Date()}
         isClearable
         showMonthDropdown
         scrollableMonthYearDropdown
+        placeholderText="Click to select a date"
       />
     </div>
   );
